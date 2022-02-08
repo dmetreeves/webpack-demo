f05005a4d3b6af713ef58d2a4b48ce842826e669
dmetreeves <mgthrone91@gmail.com>
2022-02-08 17:18:00 +0000

    add missing comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function component() {
     const element = document.createElement('div');
 
+    // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['hello', 'webpack'], ' ');
 
     return element;
