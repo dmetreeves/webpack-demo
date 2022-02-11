0ab947f493cc8ab5467f81dd6eac2c7b7eef50a5
dmetreeves <mgthrone91@gmail.com>
2022-02-11 04:20:58 +0000

    webpack-dev-server used

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
     const element = document.createElement('div');
     const btn = document.createElement('button');
 
-    element.innerHTML = _.join(['hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['hello', 'webpack2'], ' ');
 
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
