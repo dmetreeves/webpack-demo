11aba62bd51b40c86587c2dd3eb45341cbba5dbc
dmetreeves <mgthrone91@gmail.com>
2022-02-15 00:45:32 +0000

    dynamic imports async function refactored

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-function getComponent() {
-    return import('lodash')
-        .then(({ default: _ }) => {
-            const element = document.createElement('div');
-            element.innerHTML = _.join(['hello', 'webpack'], ' ');
-            return element;
-        })
-        .catch((error) => 'An error occured while loading the component');
+async function getComponent() {
+    const element = document.createElement('div');
+    const { default: _ } = await import('lodash');
+    element.innerHTML = _.join(['hello', 'webpack'], ' ');
+    return element;
 }
 
 getComponent().then((component) => {
