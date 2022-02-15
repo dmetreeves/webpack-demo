3f33a9fad6c6fbc46363eb48e3a415cc6e3a620e
dmetreeves <mgthrone91@gmail.com>
2022-02-15 00:17:11 +0000

    code splitting by entry dependencies implemented

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        'index': './src/index.js',
-        'print': './src/print.js',
-        'another': './src/another-module.js',
+        print: './src/print.js',
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared',
+        },
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared'
+        },
+        shared: 'lodash',
     },
     devtool: 'inline-source-map',
     devServer: {
@@ -22,5 +29,8 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         publicPath: '/',
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
 };
